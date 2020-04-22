24e1ffde54bf7a9d8977b2e7a00e0aff600f12f3
Meroproduction59000 <meroproduction59000@gmail.com>
2020-04-22 17:41:09 +0200

    Project initialization from SpringInitializr with Maven, Spring Boot 2.3.0 and Java 11

---
+++ src/main/java/fr/meroproduction/backendparkingcodechallenge/BackendparkingcodechallengeApplication.java
@@ -0,0 +1,13 @@
+package fr.meroproduction.backendparkingcodechallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendparkingcodechallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendparkingcodechallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/meroproduction/backendparkingcodechallenge/BackendparkingcodechallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.meroproduction.backendparkingcodechallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendparkingcodechallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
